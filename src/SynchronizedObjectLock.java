bc697ee0f9a2b4ddd3bc8998f328d08c2bef7dc0
wcrane <1191814251@qq.com>
2024-10-02 21:19:18 +0800

    feat: Synchronized对象锁

---
+++ src/SynchronizedObjectLock.java
@@ -0,0 +1,24 @@
+public class SynchronizedObjectLock implements Runnable {
+    static SynchronizedObjectLock instance = new SynchronizedObjectLock();
+
+    @Override
+    public void run() {
+        // 同步代码块形式——锁为this,两个线程使用的锁是一样的,线程1必须要等到线程0释放了该锁后，才能执行
+        synchronized (this) {
+            System.out.println("我是线程" + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "结束");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+    }
+}
